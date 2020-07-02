ccbe55717614cbf2a8fdd15ad7e7c8e9ad1a63b7
Ashirwad Satapathi <ashirwadsatapathi.aim@gmail.com>
2020-07-02 22:56:02 +0530

    Implemented a HttpTrigger for the purpose of implementing Ceaser Cipher or so called as shift cipher in a azure function called HttpTriggerCeaserCipher.

---
+++ 100DaysOfServerlessCode/Day3/BL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _100DaysOfServerlessCode.Day3
+{
+    class BL
+    {
+        public static string Encryption(string message,string key)
+        {
+            int cipherKey;
+            bool isValidKey = int.TryParse(key, out cipherKey);
+            string encryptedMessage = "";
+            char alphabet;
+            if (isValidKey)
+            {
+                foreach(char ch in message)
+                {
+                    alphabet = (char)(Convert.ToUInt16(ch) + cipherKey);
+                    encryptedMessage = encryptedMessage + alphabet;
+                }
+                return encryptedMessage;
+            }
+            else
+            {
+                return "Please enter a valid key. Any Integer is considered to be an valid key.";
+            }
+        }
+        public static string Decryption(string message, string key)
+        {
+            int cipherKey;
+            bool isValidKey = int.TryParse(key, out cipherKey);
+            string decryptedMessage = "";
+            char alphabet;
+            if (isValidKey)
+            {
+                foreach (char ch in message)
+                {
+                    alphabet = (char)(Convert.ToUInt16(ch) - cipherKey);
+                    decryptedMessage = decryptedMessage + alphabet;
+                }
+                return decryptedMessage;
+            }
+            else
+            {
+                return "Please enter a valid key. Any Integer is considered to be an valid key.";
+            }
+        }
+    }
+}
---
+++ 100DaysOfServerlessCode/Day3/HttpTriggerCeaserCipher.cs
@@ -0,0 +1,72 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace _100DaysOfServerlessCode.Day3
+{
+    public static class HttpTriggerCeaserCipher
+    {
+        [FunctionName("HttpTriggerCeaserCipher")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string message = req.Query["message"];
+            string key = req.Query["key"];
+            string operation = req.Query["Operation"];
+            
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            message = message ?? data?.message;
+            key = key ?? data?.key;
+            operation = operation ?? data.operation;
+
+            string responseMessage = "";
+
+            if (string.IsNullOrEmpty(operation))
+            {
+                responseMessage = "Please pass the operation you intend to perform.";
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(message) && string.IsNullOrEmpty(key))
+                {
+                    responseMessage = "Please pass the message and key.";
+                }
+                if (string.IsNullOrEmpty(message) && !string.IsNullOrEmpty(key))
+                {
+                    responseMessage = "Please pass the message.";
+                }
+                if (!string.IsNullOrEmpty(message) && string.IsNullOrEmpty(key))
+                {
+                    responseMessage = "Please pass the key.";
+                }
+                if (!string.IsNullOrEmpty(message) && !string.IsNullOrEmpty(key))
+                {
+                    if (operation == "Encrypt")
+                    {
+                        responseMessage = $"Encrypted message for {message} is {BL.Encryption(message, key)}";
+                    }
+                    else if(operation == "Decrypt"){
+                        responseMessage = $"Encrypted message for {message} is {BL.Decryption(message, key)}";
+                    }
+                    else
+                    {
+                        responseMessage = "Please Select a valid operation.";
+                    }
+
+                }
+            }
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
