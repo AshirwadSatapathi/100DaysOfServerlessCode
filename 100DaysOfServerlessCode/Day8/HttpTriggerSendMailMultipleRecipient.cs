d78614cf20e3a6d13fd82ac6d0d6037092ab7e7d
Ashirwad Satapathi <ashirwadsatapathi.aim@gmail.com>
2020-07-10 23:50:39 +0530

    Added the functionality to add multiple recipents to the To of an Email

--- 100DaysOfServerlessCode/Day8/HttpTriggerSendMailMultipleRecipient.cs
+++ 100DaysOfServerlessCode/Day8/HttpTriggerSendMailMultipleRecipient.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using _100DaysOfServerlessCode.Day2;
 
 namespace _100DaysOfServerlessCode.Day8
 {
@@ -14,21 +15,23 @@ namespace _100DaysOfServerlessCode.Day8
     {
         [FunctionName("HttpTriggerSendMailMultipleRecipient")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string responseMessage = "";
+            try
+            {
+                Business.CallExecute();
+                responseMessage = "Email has been succesfully sent";
+            }
+            catch
+            {
+                responseMessage = "Internal Error. Try again.";
+            }
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
+            
             return new OkObjectResult(responseMessage);
         }
     }
