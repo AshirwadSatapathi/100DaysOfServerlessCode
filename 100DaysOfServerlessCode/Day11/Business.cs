bcba14b61e25a2bdcb47b73d3f8734e944af6173
Ashirwad Satapathi <ashirwadsatapathi.aim@gmail.com>
2020-07-11 23:48:29 +0530

    Added the business for sending mails with BCC and CC.

--- 100DaysOfServerlessCode/Day11/Business.cs
+++ 100DaysOfServerlessCode/Day11/Business.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _100DaysOfServerlessCode.Day8
+namespace _100DaysOfServerlessCode.Day11
 {
     class Business
     {
@@ -23,13 +23,23 @@ namespace _100DaysOfServerlessCode.Day8
             msg.SetFrom(new EmailAddress("ashirwad.satapathi@outlook.com"));
             msg.AddTo(new EmailAddress("ashirwadsatapathi.aim@gmail.com", "Ashirwad"));
 
-            var to_emails = new List<EmailAddress>
+            var cc_emails = new List<EmailAddress>
             {
-                new EmailAddress("ashirwad@abc.com","test 1")
+                new EmailAddress("ashirwad@abc.com","test 1"),
+                new EmailAddress("abc@gmail.com","test 2")
             };
-            msg.AddTos(to_emails);
+
+            msg.AddCcs(cc_emails);
+            var bcc_emails = new List<EmailAddress> { 
+                new EmailAddress("bcd@gmail.com","test 3"),
+                new EmailAddress("cde@gmail.com","test 4")
+            };
+
+            msg.AddBccs(bcc_emails);
 
             msg.SetTemplateId("d-3e97f42b3efc437cb23f754664d41fca");
+
+
 
             var dynamicTemplateData = new ExampleTemplateData
             {
--- 100DaysOfServerlessCode/Day11/HttpTriggerSendGridCC_BCC.cs
+++ 100DaysOfServerlessCode/Day11/HttpTriggerSendGridCC_BCC.cs
@@ -19,16 +19,9 @@ namespace _100DaysOfServerlessCode.Day11
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
+            string responseMessage = "";
             return new OkObjectResult(responseMessage);
         }
     }
